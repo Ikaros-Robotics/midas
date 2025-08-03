c1ec43df07a15ece4af05fdcd21e12721ce21c68
muib <muib.akinyele@gmail.com>
2025-08-03 17:34:17 -0500

    switched to gazebo11

--- src/robot_description/launch/gazebo_launch.py
+++ src/robot_description/launch/gazebo_launch.py
@@ -1,100 +1,50 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution, Command, LaunchConfiguration 
-from launch_ros.parameter_descriptions import ParameterValue
-import os
-from ament_index_python.packages import get_package_share_directory
-from launch.actions import ExecuteProcess
+
 
 def generate_launch_description():
-    # Package paths
-    pkg_share = get_package_share_directory('robot_description')
-    
-    # Path to your SDF model
-    robot_sdf = os.path.join(pkg_share, 'urdf', 'robot.urdf')
-    
-    # World file
-    default_world = os.path.join(pkg_share, 'world', 'empty_world.sdf')
-    
-    # Launch arguments
-    world_arg = DeclareLaunchArgument(
-        'world',
-        default_value=default_world,
-        description='World file to load'
+    # these are the arguments you can pass this launch file, for example paused:=true
+    gui_arg = DeclareLaunchArgument(
+        name='gui',
+        default_value='true',
+    )
+    package_arg = DeclareLaunchArgument('urdf_package',
+                                        description='The package where the robot description is located',
+                                        default_value='robot_description')
+    model_arg = DeclareLaunchArgument('urdf_package_path',
+                                      description='The path to the robot description relative to the package root',
+                                      default_value='urdf/robot.urdf')
+
+    empty_world_launch = IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare('gazebo_ros'), 'launch', 'gazebo.launch.py']),
+
     )
 
-    # Robot State Publisher
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        parameters=[{
-            'robot_description': ParameterValue(
-                Command(['ign sdf -p ', robot_sdf]),  # Convert SDF to URDF
-                value_type=str
-            ),
-            'use_sim_time': True
-        }],
-        output='screen'
-    )
-
-    # Joint State Publisher (GUI)
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        output='screen'
-    )
-
-    # Gazebo Sim
-    gz_sim = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('ros_gz_sim'),
-                'launch',
-                'gz_sim.launch.py'
-            ])
-        ]),
+    description_launch_py = IncludeLaunchDescription(
+        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'description.launch.py']),
         launch_arguments={
-            'gz_args': ['-r -v 4 ', LaunchConfiguration('world')],
-            'on_exit_shutdown': 'true'
-        }.items()
+            'urdf_package': LaunchConfiguration('urdf_package'),
+            'urdf_package_path': LaunchConfiguration('urdf_package_path')}.items()
     )
 
-    # Spawn robot
-    spawn_robot = Node(
-        package='ros_gz_sim',
-        executable='create',
-        arguments=[
-            '-file', robot_sdf,  # Use SDF directly
-            '-name', 'midas',
-            '-allow_renaming', 'true'
-        ],
-        output='screen'
+    # push robot_description to factory and spawn robot in gazebo
+    urdf_spawner_node = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        name='urdf_spawner',
+        arguments=['-topic', '/robot_description', '-entity', 'robot', '-z', '0.5', '-unpause'],
+        output='screen',
     )
-
-    # ROS 2 Control
-    load_joint_state_broadcaster = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller',
-             '--set-state', 'active', 'joint_state_broadcaster'],
-        output='screen'
-    )
-
-    load_joint_trajectory_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state',
-             'active', 'joint_trajectory_controller'],
-        output='screen'
-    )
-
+    
     return LaunchDescription([
-        world_arg,
-        robot_state_publisher_node,
-        joint_state_publisher_node,
-        gz_sim,
-        spawn_robot,
-        #load_joint_state_broadcaster,
-        #load_joint_trajectory_controller,
+        gui_arg,
+        package_arg,
+        model_arg,
+        empty_world_launch,
+        #description_launch_py,
+        #urdf_spawner_node,
     ])
